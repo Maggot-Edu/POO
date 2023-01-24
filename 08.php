acf5c1f0d67df042a9a1759eeb7de0ce41062530
Eduard <eduardbadoyan@gmail.com>
2023-01-24 18:08:47 +0100

    08 namespaces

--- 08.php
+++ 08.php
@@ -1,4 +1,23 @@
 <?php include 'includes/header.php';
+// Icluir otras clases
 
+// require 'clases/Clientes.php';
+// require 'clases/Detalles.php';
+
+use App\Clientes;
+use App\Detalles;
+
+function mi_autoload($clase) {
+    $partes = explode('\\', $clase);
+    require __DIR__ . '/clases/' . $partes[1] . '.php';
+}
+
+spl_autoload_register('mi_autoload');
+
+
+$cliente = new Clientes();
+echo "<br>";
+$detalle = new Detalles();
+echo "<br>";
 
 include 'includes/footer.php';

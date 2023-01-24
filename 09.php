3c0cddde9f97fffeb00983bab2dfa49077eacc01
Eduard <eduardbadoyan@gmail.com>
2023-01-24 18:54:37 +0100

    statments 09

--- 09.php
+++ 09.php
@@ -1,4 +1,23 @@
 <?php include 'includes/header.php';
 
+// conectar bbdd a nysqli
+
+$db = new mysqli('localhost', 'root', '', 'bienes_crud');
+// creamos coansulta
+$query = "SELECT titulo, precio from propiedades";
+// preparamos
+$stmt = $db->prepare($query);
+// ejecutamos
+$stmt->execute();
+// creamos variable
+$stmt->bind_result($titulo, $precio);
+// asignamos el resultado
+//$stmt->fetch();
+// imprimimos
+
+while ($stmt->fetch()):
+    var_dump($titulo);
+    var_dump($precio);
+endwhile;
 
 include 'includes/footer.php';

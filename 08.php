976b9f191d5edd83bd3cdaac3e9c53e7ec204e09
Eduard <eduardbadoyan@gmail.com>
2023-01-24 18:34:18 +0100

    composer

--- 08.php
+++ 08.php
@@ -1,18 +1,10 @@
 <?php include 'includes/header.php';
 // Icluir otras clases
 
-// require 'clases/Clientes.php';
-// require 'clases/Detalles.php';
+require 'vendor/autoload.php';
 
 use App\Clientes;
 use App\Detalles;
-
-function mi_autoload($clase) {
-    $partes = explode('\\', $clase);
-    require __DIR__ . '/clases/' . $partes[1] . '.php';
-}
-
-spl_autoload_register('mi_autoload');
 
 
 $cliente = new Clientes();

e3ca318db08ec297838084232b247f7cf19d894a
Eduard <eduardbadoyan@gmail.com>
2023-01-26 13:44:10 +0100

    Fin 10

--- 10.php
+++ 10.php
@@ -1,4 +1,20 @@
 <?php include 'includes/header.php';
 
-
+// Conectar BBDD
+$db = new PDO('mysql:host=localhost; dbname=bienes_crud', 'root', '');
+// Consulta 
+$query = "SELECT titulo, precio from propiedades";
+// Preparar consulta
+$stmt = $db->prepare($query);
+// Ejecutamos consulta
+$stmt->execute();
+// Obtrenemos resultado
+$resultado = $stmt->fetchAll( PDO::FETCH_ASSOC );
+// Iterar resultado
+foreach($resultado as $propiedad):
+    echo $propiedad['titulo']. " Precio: ". $propiedad['precio'] ."<br>";
+endforeach;
+// echo "<pre>";
+// var_dump($resultado);
+// echo "</pre>";
 include 'includes/footer.php';
